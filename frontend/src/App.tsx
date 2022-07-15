baf41b78b2e9156830cb073f220300548353f6ff
lkpallarca <Ckencepallarca@gmail.com>
2022-07-15 16:04:32 +0800

    add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import Posts from './features/posts/Posts';
+
+export default function App() {
+  return (
+    <div className="App container">
+      <Posts />
+    </div>
+  )
+}
